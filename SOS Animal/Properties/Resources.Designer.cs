0e5bd029942717ce5880ac8189825d71109e838c
Giovanna <giovannarocha0522@gmail.com>
2023-06-15 09:28:05 -0300

    Botão

--- SOS Animal/Properties/Resources.Designer.cs
+++ SOS Animal/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SOS_Animal.Properties {
     // através de uma ferramenta como ResGen ou Visual Studio.
     // Para adicionar ou remover um associado, edite o arquivo .ResX e execute ResGen novamente
     // com a opção /str, ou recrie o projeto do VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -246,6 +246,16 @@ namespace SOS_Animal.Properties {
         internal static System.Drawing.Bitmap botaoVoltar {
             get {
                 object obj = ResourceManager.GetObject("botaoVoltar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fundoCachorro {
+            get {
+                object obj = ResourceManager.GetObject("fundoCachorro", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
